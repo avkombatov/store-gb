644a96f0d657b124122f0ee1ed51abec0b3063e0
kombat <avkombatov@yandex.ru>
2022-11-20 16:24:42 +0300

    fetch

--- js/main.js
+++ js/main.js
@@ -1,42 +1,59 @@
+const API = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses'
+
 // класс отвечает за генерацию отдельных товаров
 class ProductList {
+  #goods;
+ 
+
   constructor(container = '.products') {
     this.container = container; // то куда будем выводить товары
-    this._goods = []; // данные с сервера
+    this.#goods = []; // данные с сервера
     this._allProducts = [] // готовые сгенерированные экземпляры товаров
 
-    this._fetchGoods();
-    this._render();
+    this.#getProducts().then((data) => {
+      console.log(data);
+      this.#goods = [...data];
+      this._render()
+    })
+
   }
 
-  _fetchGoods() {
-    this._goods = [{
-        id: 1,
-        title: 'Notebook',
-        price: 20000
-      },
-      {
-        id: 2,
-        title: 'Mouse',
-        price: 1500
-      },
-      {
-        id: 3,
-        title: 'Keyboard',
-        price: 5000
-      },
-      {
-        id: 4,
-        title: 'Gamepad',
-        price: 4500
-      },
-    ];
+  // _fetchGoods() {
+  //   this._goods = [{
+  //       id: 1,
+  //       title: 'Notebook',
+  //       price: 20000
+  //     },
+  //     {
+  //       id: 2,
+  //       title: 'Mouse',
+  //       price: 1500
+  //     },
+  //     {
+  //       id: 3,
+  //       title: 'Keyboard',
+  //       price: 5000
+  //     },
+  //     {
+  //       id: 4,
+  //       title: 'Gamepad',
+  //       price: 4500
+  //     },
+  //   ];
+  // }
+
+  #getProducts() {
+    return fetch(`${API}/catalogData.json`)
+      .then((response) => response.json())
+      .catch((error) => {
+        console.log(error);
+      })
   }
 
   _render() {
     const block = document.querySelector(this.container)
 
-    for (let product of this._goods) {
+    for (let product of this.#goods) {
       const productObject = new ProductItem(product);
 
       this._allProducts.push(productObject);
@@ -49,9 +66,9 @@ class ProductList {
 // класс самого товара
 class ProductItem {
   constructor(product, img = 'https://via.placeholder.com/150') {
-    this.title = product.title
+    this.title = product.product_name
     this.price = product.price
-    this.id = product.id
+    this.id = product.product_id
     this.img = img
   }
 
